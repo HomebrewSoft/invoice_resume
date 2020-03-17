89dbd641d55d8116408565a46c35c1a3f934a16c
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2020-03-17 17:46:16 -0600

    [INIT]

---
+++ README.rst
@@ -0,0 +1,3 @@
+Invoice Resume
+========
+.. TODO
---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Invoice Resume',
+    'version': '0.1.0',
+    'author': 'HomebrewSoft',
+    'website': 'https://github.com/HomebrewSoft/invoice_resume',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        # security
+        # data
+        # reports
+        # views
+        'views/account_invoice.xml',
+    ],
+}
